2b7d7cd45c92fe48da6ef4ba07347dadae5f452e
TAKAHASHI Yuki <yuki.takahashi@stylez.co.jp>
2018-08-23 13:33:08 +0900

    add hostname

--- rest-api.go
+++ rest-api.go
@@ -6,15 +6,23 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"os"
 	"time"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		now := time.Now()
-		fmt.Fprintf(w, "Hello, %q, %s", html.EscapeString(r.URL.Path), now.Format("2006-01-02 15:04:05"))
+		fmt.Fprintf(w, "Hello, %q, %s\n", html.EscapeString(r.URL.Path), now.Format("2006-01-02 15:04:05"))
 
-		fmt.Fprintf(w, "\n\n# Local IP\n")
+		fmt.Fprintf(w, "\n# Host Name\n")
+		hostname, err := os.Hostname()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Fprintf(w, "%v\n", hostname)
+
+		fmt.Fprintf(w, "\n# Local IP\n")
 		addrs, err := net.InterfaceAddrs()
 		if err != nil {
 			panic(err)
